0fb70088a43000e474a1a1804548dbf80be75089
ducxinh <ducxinh.dn.201@gmail.com>
2025-05-27 17:28:26 +0700

    add code changes 2025-05-27 17:28

--- src/pages/TodoList/index.tsx
+++ src/pages/TodoList/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import { Seo } from '@/components/common/Seo/Seo';
 import { TodoList } from '@/features/todo-list/components/TodoList';
 
-const TodoListPage: React.FC = () => {
-  return <TodoList />;
+export function TodoListPage() {
+  return (
+    <>
+      <Seo title="Todo List" description="Todo List" />
+      <TodoList />
+    </>
+  )
 };
 
-export default TodoListPage;
+export default TodoListPage; 
